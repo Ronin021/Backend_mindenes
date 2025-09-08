1f8d7583437756bc6dc12a05909010ce8072a20e
Ronin021 <lavro.sandor@bolyai.local>
2025-09-08 12:42:26 +0200

    cloneozott cucc

--- 09.05/src/index.ts
+++
@@ -1,7 +0,0 @@
-console.log("Na üdv!.....")
-
-console.log("Na üdv!.....de más")
-
-console.log("Csá")
-
-console.log("Csá de másmilyen csá")
--- 09.05/valami2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Na üdv!.....");
-console.log("Na üdv!.....de más");
-console.log("Csá");
-console.log("Csá de másmilyen csá");
-//# sourceMappingURL=valami2.js.map
