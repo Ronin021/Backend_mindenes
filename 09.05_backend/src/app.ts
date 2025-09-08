3e5b5ff973df6c581102a3bc96e2f264788b8c29
Ronin021 <lavro.sandor@BOLYAI.LOCAL>
2025-09-08 13:25:21 +0200

    Backend cuccok masodik óra

---
+++ 09.05_backend/src/app.ts
@@ -0,0 +1,12 @@
+import {data} from "./data"
+
+
+console.log("Na üdv!.....")
+
+console.log("Na üdv!.....de más")
+
+console.log("Csá")
+
+console.log("Csá de másmilyen csá")
+
+console.log(data)
---
+++ 09.05_backend/valami2.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Na üdv!.....");
+console.log("Na üdv!.....de más");
+console.log("Csá");
+console.log("Csá de másmilyen csá");
+//# sourceMappingURL=valami2.js.map
