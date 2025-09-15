c8f2b51e2ac4775f28b568c1ce2161553f98bf70
Ronin021 <lavro.sandor@BOLYAI.LOCAL>
2025-09-15 13:54:32 +0200

    Ujj cuccli

---
+++ backendcuccok/typescript/src/index.ts
@@ -0,0 +1,70 @@
+import{data} from "./data"
+import {IKutya}  from "./kutya";
+ 
+
+ 
+ 
+function createHTMLElementwithParentElement(htmltag:string, parent:HTMLElement ){
+    const htmlelem = document.createElement(htmltag);
+    parent.appendChild(htmlelem);
+ 
+    return htmlelem;
+}
+ 
+function createHTMLElementwithParentElementandInnerHtml(htmltag:string, parent:HTMLElement, innerhtml: string | number | boolean | null ){
+    const htmlelem = document.createElement(htmltag);
+    htmlelem.innerHTML = String(innerhtml)
+    parent.appendChild(htmlelem);
+ 
+    return htmlelem;
+}
+ 
+const table = createHTMLElementwithParentElement('table', document.body);
+const tfej = createHTMLElementwithParentElement('thead', table);
+const tbody = createHTMLElementwithParentElement('tbody', table);
+ 
+const tfejsor = createHTMLElementwithParentElement("tr", tfej);
+ 
+const fejtomb = [
+    {
+        innerhtml: "id"
+    },
+    {
+        innerhtml: "nev"
+    },
+    {
+        innerhtml: "fajta"
+    },
+    {
+        innerhtml: "nem"
+    },
+    {
+        innerhtml: "eletkor"
+    },
+    {
+        innerhtml: "kepUrl"
+    }
+];
+ 
+for(const fejlcell of fejtomb){
+    createHTMLElementwithParentElementandInnerHtml('th', tfejsor, fejlcell.innerhtml);
+ 
+}
+ 
+function renderingDogTable(data1: IKutya[]){
+    for (const tbodysor of data1) {
+        const elemek = [tbodysor.id, tbodysor.nev, tbodysor.fajta, tbodysor.nem, tbodysor.eletkor, tbodysor.kepUrl];
+        const trtb = createHTMLElementwithParentElement('tr', tbody);
+        for (const elem of elemek) {
+            createHTMLElementwithParentElementandInnerHtml("td", trtb, elem);
+        }
+   
+    }
+};
+ 
+renderingDogTable(data);
+ 
+//const kutya : Kutya =  new Kutya(data[0]);
+ 
+//console.log(kutya.dogs(data));
+ 
---
+++ backendcuccok/typescript/src/kutya.ts
@@ -0,0 +1,83 @@
+export interface IKutya{
+    id: number | null;
+    nev: string;
+    fajta: string;
+    nem: boolean;
+    eletkor: number;
+    kepUrl: string | null;
+
+}
+
+
+interface Config{
+    color?: string;
+    width?: number;
+    [propName: string]: unknown;
+    }
+    
+     function createValami(config: Config){
+        return{
+            config
+        }
+     } 
+
+     let options = {area:"cakhds"}
+     let valamiConfig = createValami(options);
+        console.log(valamiConfig);
+
+
+
+
+
+
+
+class Valami{
+    public name! : string;
+  
+    x : number;
+    y : number;
+
+    constructor(  y : number, x = 0){
+        this.x = x;
+        this.y = y;
+    }
+
+};
+
+const valami = new Valami(5);
+//valami.x = 15;
+console.log(`${valami.x}, ${valami.y}`)
+export default class Kutya implements IKutya{
+    id: number | null;
+    nev: string;
+    fajta: string;
+    nem: boolean;
+    eletkor: number;
+    kepUrl: string | null;
+
+    constructor(dog: IKutya){
+        this.id = dog.id || null;
+        this.nev = dog.nev;
+        this.fajta = dog.fajta;
+        this.nem = dog.nem;
+        this.eletkor = dog.eletkor;
+        this.kepUrl = dog.kepUrl || null
+
+    }
+
+    get Id(){
+        return this.id
+    }
+    set Id(id:number | null){
+        this.id = id
+    }
+
+    public dogs(dogs1: IKutya[]): IKutya[] {
+        const dogs: IKutya[] = [];
+        dogs.push(...dogs1)
+        return dogs;
+    }
+};
+
+
+
