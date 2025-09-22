8b14b97f0417eaa4d1cd492846e7a221276993fc
Ronin021 <lavro.sandor@BOLYAI.LOCAL>
2025-09-22 14:59:33 +0200

    Kiír 1 kis aranyos dolgot, elvesztem utna

--- backendcuccok/src/app.ts
+++ backendcuccok/src/app.ts
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+
+
 app.use(express.json());
 
+
+app.get("/api", (req, res) => {
+    req
+    res.status(200).send("Brr Brr patapim");
+ console.log("hhdhdhdhd");
+});
+
+const valasz = (res:any, req:any) =>{
+
+}
 export default app;
